5b30f062f0831293fd0a65d7fb5c2d670fe3145f
zoexiong <xiongsy@uw.edu>
2016-08-25 16:48:57 -0700

    add files for admin page for testing

---
+++ admin.md
@@ -0,0 +1,11 @@
+---
+layout: admin
+title: "Admin"
+title_bar: "title_bar"
+title_secondary: "title_secondary"
+
+angular_includes:
+  - "{{ site.baseurl }}/app/tractdbConfig.js"
+  - "{{ site.baseurl }}/app/listAccountsApp.js"
+
+---
---
+++ app/listAccountsApp.js
@@ -0,0 +1,20 @@
+var app = angular.module('listAccountsApp', ['tractdb.config']);
+
+app.controller(
+    'listAccountsController',
+    [
+        '$scope', '$http', 'BASEURL_PYRAMID',
+        function ($scope, $http) {
+            $http({
+                method: 'GET',
+                url: BASEURL_PYRAMID + '/accounts',
+                headers: {'Content-Type': 'application/json'},
+                data: $scope.viewModel
+            }).then(function onSuccess(response) {
+                $scope.accounts = response.data;
+            }, function onError(response) {
+                $scope.error = response.statusText;
+            });
+        }
+    ]
+);
